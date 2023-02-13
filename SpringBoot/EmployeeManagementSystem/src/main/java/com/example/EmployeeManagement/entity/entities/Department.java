10877f7263adb0ea1e11a2a9500c66bc4eb5a25f
vishnuprakashN <90830491+vishnuprakashN@users.noreply.github.com>
2023-02-13 16:19:31 +0530

    updated

--- SpringBoot/EmployeeManagementSystem/src/main/java/com/example/EmployeeManagement/entity/entities/Department.java
+++ SpringBoot/EmployeeManagementSystem/src/main/java/com/example/EmployeeManagement/entity/entities/Department.java
@@ -3,6 +3,8 @@ package com.example.EmployeeManagement.entity.entities;
 import com.example.EmployeeManagement.entity.relations.EmployeeSkills;
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
